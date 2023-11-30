adfd81a25c85e7a6e5e515086976deaceaf6e1d2
Mathieu Barboteau <mathieu.barboteau@ynov.com>
2023-11-30 11:39:03 +0100

    fix main CLEAN TODO

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 from __future__ import annotations
 from random import randint, choice, shuffle
 from rich.console import Console
+from rich import print
 from Attackers import Samurai, Berserker, Mage, Thief, Warrior
 from Tank import Paladin, Leviathan, Phantom_Warden, Shield_Master, Guardian
 from Dice import Dice
 
-_game_dice = randint(4, 9)
-_team_len = 1000 # by class (2 to 12*_max_instances)
-_max_instances = 266 # of each class
 
 class Team:
+    _game_dice = 6
+    _max_instances = 1 # of each class
+    _team_len = 5 # by type_class (= class file name) (1 to 5*_max_instances)
+    
     def __init__(self, is_red_team: bool):
         self.is_red_team = is_red_team
-        self.tanks = self._create_team([Paladin, Leviathan, Phantom_Warden, Shield_Master, Guardian], _max_instances)  # Guardian
-        self.attackers = self._create_team([Samurai, Berserker, Mage, Warrior, Thief], _max_instances)  # Thief
-
+        self.tanks = self._create_team([Paladin, Leviathan, Phantom_Warden, Shield_Master, Guardian], self._max_instances)
+        self.attackers = self._create_team([Samurai, Berserker, Mage, Thief, Warrior], self._max_instances)
     def _create_team(self, character_classes, max_instances):
         character_instances = []
 
         for character_class in character_classes:
-            instances = [character_class(f"{character_class.__name__}_n°{i+1}", Dice(_game_dice)) for i in range(max_instances)]
+            instances = [character_class(f"{character_class.__name__} n°{i+1} {'[bold blue]BLUE Team[/bold blue]' if not self.is_red_team else '[bold red]RED Team[/bold red]'}", Dice(self._game_dice)) for i in range(max_instances)]
             character_instances.extend(instances)
 
         shuffle(character_instances)
-        return character_instances[:_team_len]
+        return character_instances[:self._team_len]
 
 if __name__ == "__main__":
     red_team = Team(is_red_team=True)
     blue_team = Team(is_red_team=False)
 
     red_attacker_turn = choice([True, False])
-
+    
+    # log every fighters (first team of attackers chosed)
+    
     while any(char.is_alive() for char in red_team.attackers + red_team.tanks) and any(char.is_alive() for char in blue_team.attackers + blue_team.tanks):
         if red_attacker_turn:
             for attacker in red_team.attackers:
@@ -50,11 +53,13 @@ if __name__ == "__main__":
                 console.print("[bold red]Red Team wins![/bold red]")
                 for char in red_team.attackers + red_team.tanks:
                     if char.is_alive():
-                        console.print(char)
+                        chara = char.__str__()
+                        console.print(chara)
                         char.show_healthbar()
                 for char in red_team.attackers + red_team.tanks:
                     if not char.is_alive():
-                        console.print(char)
+                        chara = char.__str__()
+                        console.print(chara)
                         char.show_healthbar()
                 break
 
@@ -76,12 +81,27 @@ if __name__ == "__main__":
                 console.print("[bold blue]Blue Team wins![/bold blue]")
                 for char in blue_team.attackers + blue_team.tanks:
                     if char.is_alive():
-                        console.print(char)
+                        chara = char.__str__()
+                        console.print(chara)
                         char.show_healthbar()
                 for char in blue_team.attackers + blue_team.tanks:
                     if not char.is_alive():
-                        console.print(char)
+                        chara = char.__str__()
+                        console.print(chara)
                         char.show_healthbar()
-                        break
-
+                break
+        if not any(char.is_alive() for char in blue_team.attackers + blue_team.tanks):
+                console = Console()
+                console.print("[bold red]Red Team wins![/bold red]")
+                for char in red_team.attackers + red_team.tanks:
+                    if char.is_alive():
+                        chara = char.__str__()
+                        console.print(chara)
+                        char.show_healthbar()
+                for char in red_team.attackers + red_team.tanks:
+                    if not char.is_alive():
+                        chara = char.__str__()
+                        console.print(chara)
+                        char.show_healthbar()
+                break
         red_attacker_turn = not red_attacker_turn
