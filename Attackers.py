b3602e2b514262d9d1419cacad56917b4d88a1c7
mmederic <mederic.marquie@ynov.com>
2023-11-30 09:05:54 +0100

    update Tank & Attacker self._name

--- Attackers.py
+++ Attackers.py
@@ -12,15 +12,15 @@ class Warrior(Character):
     def compute_damages(self, roll, target):
         damages = super().compute_damages(roll, target)
         if roll == self._dice._faces:
-            print(f"🪓 Bonus: Axe in your face ({self._attack_bonus} attack)")
+            print(f"🪓 Bonus: {self._name} Axe in your face ({self._attack_bonus} attack)")
             damages += self._attack_bonus
             self._attack_bonus += self._attack_bonus
         elif roll == 1:
-            self.console.print(f"🩸 The roll result is {roll}. You FAILED ! The axe comes back straight to the face. {self._name} takes {self._attack_bonus} damages.")
+            self.console.print(f"🩸 The roll result is {roll}. You FAILED ! The {self._name} axe comes back straight to the face. {self._name} takes {self._attack_bonus} damages.")
             self.decrease_health(self._attack_bonus)
             damages = 0
         else:
-            self.console.print(f"🎲 The roll result is {roll}. No additional damage or life steal this time!")
+            self.console.print(f"🎲 The roll result is {roll}. No additional damage or life steal this time for {self._name} !")
 
         return damages
 
@@ -32,21 +32,21 @@ class Thief(Character):
     _life_steal = randint(2, 5)
 
     def compute_damages(self, roll, target: Character):
-        self.console.print(f"🔪 Bonus: Sneaky attack (ignore defense: + {target.get_defense_value()} bonus)")
+        self.console.print(f"🔪 Bonus: {self._name} Sneaky attack (ignore defense: + {target.get_defense_value()} bonus)")
         damages = super().compute_damages(roll, target) + target.get_defense_value()
 
         if roll == self._dice._faces: 
             if self._current_health < self._max_health:
                 self._current_health = min(self._current_health + self._life_steal, self._max_health)
-                self.console.print(f"👤  Additional damage! Thief performs Life steal, gaining {self._life_steal} life.")
+                self.console.print(f"👤  Additional damage! {self._name} performs Life steal, gaining {self._life_steal} life.")
                 self.show_healthbar()
                 return damages
         elif roll == 1:
             self._life_steal = max(0, self._life_steal - 2)
-            self.console.print(f"🩸 The roll result is {roll}. You FAILED! Thief's life steal reduced by {2}.")
+            self.console.print(f"🩸 The roll result is {roll}. You FAILED! {self._name}'s life steal reduced by {2}.")
             return damages
         else:
-            self.console.print(f"🎲 The roll result is {roll}. No additional damage or life steal this time!")
+            self.console.print(f"🎲 The roll result is {roll}. No additional damage or life steal this time for {self._name} !")
             
         return damages
 
@@ -62,17 +62,17 @@ class Berserker(Character):
         damages = super().compute_damages(roll, target)
         if self._current_health < self._max_health:
             
-            self.console.print(f"🎭 Berserker is enraged ! Attack bonus: +{self._rage_boost*2}")
+            self.console.print(f"🎭 {self._name} is enraged ! Attack bonus: +{self._rage_boost*2}")
             return damages + self._rage_boost*2
         
         if roll == self._dice._faces:
-            self.console.print(f"Additional damage! Attack bonus: {max(2,self._rage_boost)}")
+            self.console.print(f" {self._name} got additional damage! Attack bonus: {max(2,self._rage_boost)}")
             return damages + max(2,self._rage_boost)
         elif roll == 1:
-            self.console.print(f"🩸  The roll result is {roll}. You FAILED! Attack reduced by {max(2,self._rage_boost)}.")
+            self.console.print(f"🩸  The {self._name} roll result is {roll}. You FAILED! Attack reduced by {max(2,self._rage_boost)}.")
             return damages - max(2,self._rage_boost)
         else:
-            self.console.print(f"🎲 The roll result is {roll}. No additional damages !")
+            self.console.print(f"🎲 The {self._name} roll result is {roll}. No additional damages !")
         return damages
 
  
@@ -85,16 +85,16 @@ class Samurai(Character):
     def compute_damages(self, roll, target: Character):
         damage = super().compute_damages(roll, target)
         if roll == self._dice._faces:
-            self.console.print(f"💉 Bleeding attack! Inflicting additional damage over time. Roll = {roll}. Bonus {self._bleed_damage} attack")
+            self.console.print(f"💉 Bleeding attack! {self._name} inflict additional damage over time. Roll = {roll}. Bonus {self._bleed_damage} attack")
             damage += self._bleed_damage
             self._bleed_damage += self._bleed_damage
             return damage
         elif roll == 1:
-            print(f"🩸 The roll result is {roll}. You FAILED! Samurai inflicts self-damage, losing {self._bleed_damage if self._bleed_damage <= self._current_health - self._bleed_damage else self._current_health} HP.")
+            print(f"🩸 The roll result is {roll}. You FAILED! {self._name} inflicts self-damage, losing {self._bleed_damage if self._bleed_damage <= self._current_health - self._bleed_damage else self._current_health} HP.")
             self.decrease_health(self._bleed_damage)
             return 0
         else:
-            self.console.print(f"🎲 The roll result is {roll}. No additional damages this time!")
+            self.console.print(f"🎲 The roll result is {roll}. No additional damages for {self._name} this time!")
         return damage
 
 
@@ -119,10 +119,10 @@ class Mage(Character):
 
 
         elif roll == 1:
-            self.console.print(f"🩸 The roll result is {roll}. You FAILED! Mage's Fireball backfires, inflicting self-damage.")
+            self.console.print(f"🩸 The roll result is {roll}. You FAILED! {self._name}'s Fireball backfires, inflicting self-damage.")
             self.decrease_health(self.target_defense_reduction)
             return damage
         else:
-            self.console.print(f"🎲 The roll result is {roll}. The Fireball does not inflict Burn!")
+            self.console.print(f"🎲 The roll result is {roll}. The {self._name}'s Fireball does not inflict Burn!")
         return damage
 
--- Tank.py
+++ Tank.py
@@ -15,23 +15,23 @@ class Paladin(Character):
                 if amount <= 0 :
                     amount = 0
                 else: 
-                    self.console.print(" 🔨🛡️ Bonus : Holy Armor reduces wounds by 3")
+                    self.console.print(f" 🔨🛡️ Bonus : {self._name} Holy Armor reduces wounds by 3")
                     amount -= 3
             elif rollD4 == 2:
                 randomAttack = randint(-3,5)
                 self._attack_value = self._attack_value + randomAttack
-                self.console.print(f" 💢🔨✨ Bonus : The Paladin calls to the Holy light to gain attack, {self._attack_value} : {randomAttack}")
+                self.console.print(f" 💢🔨✨ Bonus : {self._name} calls to the Holy light to gain attack, {self._attack_value} : {randomAttack}")
             elif rollD4 == 3:
                 randomDefense = randint(-1,7)
                 self._defense_value = self._defense_value + randomDefense
-                self.console.print(f" 🛡️🔨✨ Bonus : The Paladin calls to his faith to gain defense, {self._defense_value} : {randomDefense}")
+                self.console.print(f" 🛡️🔨✨ Bonus : {self._name} calls to his faith to gain defense, {self._defense_value} : {randomDefense}")
             else:
                 randomFaith = randint(-1,10)
                 if (self._current_health + randomFaith >= self._max_health):
                         self._current_health = self._max_health
                 else:
                     self._current_health += randomFaith  
-                    self.console.print(f" ✨🔨✨ Bonus : The Paladin heals himself thanks to his faith, {self._current_health} : {randomFaith}")
+                    self.console.print(f" ✨🔨✨ Bonus : {self._name} heals himself thanks to his faith, {self._current_health} : {randomFaith}")
                     
         return super().decrease_health(amount)
       
@@ -43,14 +43,14 @@ class Phantom_Warden(Character):
     def compute_wounds(self, damages, roll, attacker):
        roll: int = self._dice.roll()
        if (roll == self._dice._faces):
-           self.console.print(" 🌫️ 👻💨 Bonus : The Phantom_Warden evaporated throught the attack ! (Wounds : 0)")
+           self.console.print(f" 🌫️ 👻💨 Bonus : {self._name} evaporated throught the attack ! (Wounds : 0)")
            return super().compute_wounds(0, 0, attacker)   
        elif (roll == 1):
-           self.console.print("🔻👻 Malus : The Phantom_Warden missed is escape ... (Wounds x 2)")
+           self.console.print(f"🔻👻 Malus : {self._name} missed is escape ... (Wounds x 2)")
            return super().compute_wounds(damages*2, roll, attacker)
 
        else:
-           self.console.print(f" 👻💨 Bonus : The Phantom_Warden evaded the attack ! (Wounds - {roll})")
+           self.console.print(f" 👻💨 Bonus : {self._name} evaded the attack ! (Wounds - {roll})")
            if round(super().compute_wounds(damages, roll, attacker) - roll) <= 0:
                 return 0
            else:
@@ -65,20 +65,20 @@ class Leviathan(Character):
         if (self._current_health < self._max_health):
            coinThrow = randint(1,2)
            if coinThrow == 1:
-               self.console.print("🔻🐙 Malus : The Leviathan failed to restore health points")            
+               self.console.print(f"🔻🐙 Malus : {self._name} failed to restore health points")            
            else : 
                restore_roll = randint(1,10)
                if (restore_roll == 10):
-                    self.console.print("✨🐙 Bonus : The Leviathan restored 4 health points")
+                    self.console.print(f"✨🐙 Bonus : {self._name} restored 4 health points")
                     if (self._current_health + restore_roll > self._max_health):
                         self._current_health = self._max_health
                     else:
                         self._current_health += 10  
                elif (restore_roll == 1):
-                  self.console.print(" 🔻🐙 Malus : The Leviathan didn't succeed to restore health points")
+                  self.console.print(f" 🔻🐙 Malus : {self._name} didn't succeed to restore health points")
                   
                else:
-                    self.console.print(f"✨🐙 Bonus : The Leviathan restored {restore_roll} health points")
+                    self.console.print(f"✨🐙 Bonus : {self._name} restored {restore_roll} health points")
                     if (self._current_health + restore_roll > self._max_health):
                         self._current_health = self._max_health
                     else:
@@ -100,26 +100,26 @@ class Guardian(Character): # Enforcer
             rollD4 = randint(1,4)
             if not rollD4 == 4:
                 # decrease health
-                self.console.print(" 🔻♖ The Guardian failed to gain defense as he's weakened by the Attacker")
+                self.console.print(f" 🔻♖ {self._name} failed to gain defense as he's weakened by the Attacker")
                 super().decrease_health(amount) 
             else:
                 if self._current_health - amount > 0 and amount -1 > 0:
                     self._defense_value += 2
-                    self.console.print(f" 🔥♖ The Guardian enrages and gain {2} defense points !")
+                    self.console.print(f" 🔥♖ {self._name} enrages and gain {2} defense points !")
                 super().decrease_health(amount) 
-            self.show_healthbar()
-            amount = max(0, amount) 
+            # self.show_healthbar()
+            # amount = max(0, amount) 
                    
 class Shield_Master(Character):
    def compute_wounds(self, damages, roll, attacker: Character):
         rollD6 = randint(1,6)
         if rollD6 == 1:
-            self.console.print(" 🔻🛡️ Malus : The Shield_Master didn't succeeded in his parry")
+            self.console.print(f" 🔻🛡️ Malus : {self._name} didn't succeeded in his parry")
             return super().compute_wounds(damages, roll, attacker)
         elif rollD6 == 6:
-            self.console.print(f" 💢🛡️ Bonus : The Shield_Master parried your attack for a Critical Hit ! (Sends back {damages})")
+            self.console.print(f" 💢🛡️ Bonus : {self._name} parried your attack for a Critical Hit ! (Sends back {damages})")
             attacker.decrease_health(damages) 
             return 0
         else:
-            self.console.print(f" 💫🛡️ Bonus : The Shield_Master parried your attack ! ({damages}/{rollD6} ≈ {round(damages/rollD6)})")
+            self.console.print(f" 💫🛡️ Bonus : {self._name} parried your attack ! ({damages}/{rollD6} ≈ {round(damages/rollD6)})")
             return super().compute_wounds(round(damages/rollD6), roll, attacker)
