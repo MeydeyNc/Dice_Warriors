2674fc358b2c99cc4683c90f8917ec60b6a6504c
Mathieu Barboteau <mathieu.barboteau@ynov.com>
2023-12-02 15:44:20 +0100

    normal mode

--- Character.py
+++ Character.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 from random import randint
 from rich.console import Console
 
-from advanced import ask_user
+
+# Do not remove blanck ! 
 
 class Character:
-    _max_health = ask_user("Choose default max health value :   ")
+    _max_health = randint(18, 22)
     _current_health = _max_health
-    _attack_value = ask_user("Choose default attack value, def will be attack/3 :   ")
-    _defense_value = round(_attack_value / 3) + randint(-2, 3)
-
+    _attack_value = randint(7, 13)
+    _defense_value = randint(2, 5)
     
     def __init__(self, name: str, dice) -> None:
         self._name = name
